d960d37ff830600465d842b3dac75c942d917b40
CamiloDev22 <kamilo042.cp@gmail.com>
2023-09-17 17:17:00 -0500

    code for toolbar component ready

---
+++ to-do-list/src/components/Toolbar.js
@@ -0,0 +1,13 @@
+// src/components/Toolbar.js
+import React from 'react';
+
+function Toolbar() {
+  return (
+    <div className="toolbar">
+      <button>Create Task</button>
+      <button>Delete  Task</button>
+    </div>
+  );
+}
+
+export default Toolbar;
